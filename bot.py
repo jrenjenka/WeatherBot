9005d9b0c203e042bb342839fdec567d78f890df
Liubov <lbvsereda@gmail.com>
2017-07-17 08:31:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def main():
                 api.update_status(tweet)
                 
         # delay next tweet for 3 hours
-        time.sleep(10800000)
+        time.sleep(10800)
 
 if __name__ == "__main__":
     main()
